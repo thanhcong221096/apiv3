fb0ebec74edaeb500f3e51062b927347c66c5931
thanhcong221096 <116239099+thanhcong221096@users.noreply.github.com>
2022-10-20 17:06:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server port ${PORT}`));
+
+app.get('/', (req, res) => {
+	res.status(200).send(`
+	  Callcenter API is built by NTT Networks</br>
+    Port: ${PORT}</br>`);
+});
